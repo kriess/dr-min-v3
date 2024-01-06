2f736effdad55611e2e0074ebf2be208b3c0b6b6
Ken Riess <ken.riess@nfl.com>
2024-01-06 07:43:09 -0800

    feat: text updates

--- pages/services/body/thigh-lift.vue
+++ pages/services/body/thigh-lift.vue
@@ -21,21 +21,22 @@ useHead({
     <div class="services-section">
       <div class="services-title">Thigh Lift</div>
       <services-body-main-thumb
-        src="/img/services/thumbs/thighlift.jpg">
-      </services-body-main-thumb>
+        src="/img/services/thumbs/thighlift.jpg"
+      ></services-body-main-thumb>
       <p>
         A thigh lift is performed to remove excess skin and fat from the upper
         legs. Candidates typically suffer from loose, inelastic skin after
-        extreme weight loss due to diet, exercise or bariatric surgery. Because
-        of this, the procedure is often combined with others such as breast
-        lifts, facelifts and arm lifts to retain an attractive body proportion.
+        extreme weight loss due to diet, exercise or bariatric surgery.
       </p>
       <p>
-        In a medial thigh lift (inner thigh lift), the surgical incision is made
-        in the inner thigh along the line where the leg meets the groin. In a
-        lateral thigh lift (outer thigh lift), the incision is made from the
-        groin around the back of the body, so that the buttocks are lifted along
-        with the thigh.
+        There are two primary types of thigh lift procedures. In a medial thigh
+        lift, the surgical incision is made on the inner thigh along the line
+        where the leg meets the groin or it can extend vertically down the inner
+        thigh. This approach focuses on addressing sagging skin and excess fat
+        on the inner thighs. On the other hand, a lateral thigh lift involves an
+        incision made from the groin area around the back of the body. This
+        technique not only targets the outer thighs but also provides a lifting
+        effect on the buttocks. Liposuction may also be used to enhance results.
       </p>
     </div>
   </div>
@@ -44,6 +45,7 @@ useHead({
 <style lang="scss" scoped>
 .services-details {
   width: auto;
+
   .services-title {
     display: none;
     color: $primary;
@@ -51,21 +53,26 @@ useHead({
     font-weight: bold;
     margin-bottom: 20px;
   }
+
   p {
     margin-bottom: 20px;
   }
+
   .header-text {
     font-size: 150%;
     color: $primary;
     font-weight: bold;
     margin-bottom: 20px;
   }
+
   dt {
     font-weight: bold;
   }
+
   dd {
     margin-bottom: 20px;
   }
+
   .thumb {
     border-radius: $border-radius-v3;
     border: 1px solid #ccc;
@@ -73,8 +80,10 @@ useHead({
     float: right;
     width: 50%;
   }
+
   .unordered-list {
     margin-bottom: 20px;
+
     li {
       margin-left: 20px;
     }
